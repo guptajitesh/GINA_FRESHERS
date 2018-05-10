af4f70b2493c7a3839c82382844b78367304731b
Aditya <singhadityaftp@gmail.com>
2018-05-10 17:44:46 +0530

    Change in java File

--- hello.java
+++ hello.java
@@ -1,1 +1,17 @@
-System.out.println("Hello World");
+if(num4.getText().toString().equals(""))
+                 {
+                     flag=1;
+                     n2=0;
+                 }
+                 else
+                 {
+                     try{
+                         n2=Integer.parseInt(num4.getText().toString());
+                         flag=1;
+                     }
+                     catch (NumberFormatException e)
+                     {
+                         flag=0;
+                         n2=0;
+
+                     }
